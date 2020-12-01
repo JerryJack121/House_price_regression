258a80d6e70c38440c7329d9d6f715beec1cb099
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2020-12-01 11:15:51 +0800

    Initial commit

---
+++ fusion2.py
@@ -0,0 +1,157 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import copy
+from torch.optim import lr_scheduler
+from Net.model import fusion_net
+from torch.autograd import Variable
+
+# 設定 GPU
+device = torch.device("cuda")
+
+# 載入數據
+ada_df = pd.read_csv('./csv/adaboost_fold_val.csv')
+svm_df = pd.read_csv('./csv/svm_fold_val.csv')
+fore_df = pd.read_csv('./csv/forest_fold_val.csv')
+knn_df = pd.read_csv('./csv/knn_fold_val.csv')
+xgb_df = pd.read_csv('./csv/xgb_fold_val.csv')
+
+train_x = torch.tensor(np.hstack((ada_df.values[:10000, 2:3], fore_df.values[:10000, 2:3], knn_df.values[:10000, 2:3], xgb_df.values[:10000, 2:3], svm_df.values[:10000, 2:3])),
+                        dtype=torch.float).to(device)
+
+val_x = torch.tensor(np.hstack((ada_df.values[10000:, 2:3], fore_df.values[10000:, 2:3], knn_df.values[10000:, 2:3], xgb_df.values[10000:, 2:3], svm_df.values[10000:, 2:3])),
+                        dtype=torch.float).to(device)
+
+
+train_y = torch.tensor(ada_df.values[:10000, 1:2], dtype=torch.float).to(device)
+val_y = torch.tensor(ada_df.values[10000:, 1:2], dtype=torch.float).to(device)
+
+ada_test_df = pd.read_csv('./csv/adaboost_fold_test.csv')
+svm_test_df = pd.read_csv('./csv/svm_fold_test.csv')
+fore_test_df = pd.read_csv('./csv/forest_fold_test.csv')
+knn_test_df = pd.read_csv('./csv/knn_fold_test.csv')
+xgb_test_df = pd.read_csv('./csv/xgb_fold_test.csv')
+test = torch.tensor(np.hstack((ada_test_df.values[:, 6:7], fore_test_df.values[:, 6:7], knn_test_df.values[:, 6:7], xgb_test_df.values[:, 6:7], svm_test_df.values[:, 6:7])), 
+                        dtype=torch.float).to(device)
+
+# 載入預訓練模型
+pre_model = torch.load('./weight/fusion_1127_adamw.pth')
+pretrained_dict = pre_model.state_dict()
+model = fusion_net(features=train_x.shape[1]).to(device)
+model_dict = model.state_dict()
+# filter out unnecessary keys
+pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+# overwrite entries in the existing state dict
+model_dict.update(pretrained_dict)
+model.load_state_dict(model_dict)
+
+
+# 選擇Loss function
+mse = nn.MSELoss(reduction='mean')
+SMOOTHL1LOSS = torch.nn.SmoothL1Loss()
+Loss = SMOOTHL1LOSS
+# 選擇優化器
+optimizer = torch.optim.SGD(params=model.parameters(),
+                            lr=1e-7,
+                            momentum=0.9)  
+
+scheduler = lr_scheduler.StepLR(optimizer, step_size=300, gamma=0.9)
+
+train_x = Variable(train_x)
+train_y = Variable(train_y)
+# 設定訓練批次資料
+batch_size = 5000
+train_dataset = torch.utils.data.TensorDataset(train_x,
+                                                train_y)
+train_iter = torch.utils.data.DataLoader(train_dataset,
+                                            batch_size,
+                                            shuffle=True)
+
+# 訓練與驗證
+train_losses = []
+val_losses = []
+switch = True
+
+# 計算初始損失
+model.eval()  # 設定模型驗證模式
+with torch.no_grad():
+    optimizer.zero_grad()
+    y_pred = model(val_x)
+    loss = Loss(y_pred, val_y)
+    print('初始loss:',loss.item())
+
+num_epochs = 1000
+for epoch in range(num_epochs):
+    print('-' * 100)
+    print('Epoch {}/{}'.format(epoch + 1, num_epochs))
+    # 訓練階段
+    for train_betch_x, train_betch_y in train_iter:
+        model.train()  # 設定模型訓練模式
+        y_pred = model(train_betch_x)
+        loss = Loss(y_pred, train_betch_y)
+        if torch.isnan(loss):
+            print('break')
+            break
+        train_running_loss = loss.item()
+        # PyTorch的backward()方法計算梯度，預設會將每次的梯度相加，所以必須先歸零
+        optimizer.zero_grad()
+        # 反向傳播
+        loss.backward()
+        # 根據反向傳播得到的梯度更新模型參數
+        optimizer.step() 
+        scheduler.step()
+
+        # 驗證階段
+        model.eval()  # 設定模型驗證模式
+        with torch.no_grad():
+            optimizer.zero_grad()
+            y_pred = model(val_x)
+            loss = Loss(y_pred, val_y)
+            val_running_loss = loss.item()
+        # 複製最好的模型參數資料
+        if switch == True:
+            best_val_loss = val_running_loss
+            best_model_wts = copy.deepcopy(model.state_dict())
+            best_epoch = 0
+            switch = False
+        if val_running_loss < best_val_loss:
+            best_epoch = epoch
+            best_val_loss = val_running_loss
+            best_model_wts = copy.deepcopy(model.state_dict())
+    train_losses.append(train_running_loss)
+    val_losses.append(val_running_loss)
+    lr = optimizer.state_dict()['param_groups'][0]['lr']
+    print('train loss: {:4f} / val loss: {:4f} / Best_val_loss: {:4f} / best_epoch於 {} / Learning_rate: {}'.format(train_running_loss,
+                                                                                            val_running_loss, 
+                                                                                            best_val_loss,
+                                                                                            best_epoch,
+                                                                                            lr))
+
+# 儲存模型
+model.load_state_dict(best_model_wts)
+torch.save(model, './weight/fusion_1127_sgd.pth')
+
+# 繪出loss下降曲線
+plt.figure()
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.plot(train_losses, label='train_losses')
+plt.plot(val_losses, label='val_losses')
+plt.legend(loc='best')
+plt.plot()
+plt.show()
+
+# 預測結果
+model = torch.load('./weight/fusion_1127_sgd.pth')
+with torch.no_grad():
+    predictions = model(test).cpu().detach().numpy()
+    my_submission = pd.DataFrame({
+        'id':
+        pd.read_csv(r'D:\DATASET\ntut-ml-regression-2020\test-v3.csv').id,
+        'price':
+        predictions[:, 0]
+    })
+    my_submission.to_csv('./csv/fusion_1127_sgd.csv', index=False)
+    print('寫入預測結果')
